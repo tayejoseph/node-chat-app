8192d5febad11a38c757334fb6e3b429981e3d30
Taye <tayejoseph6@gmail.com>
2018-04-21 00:03:36 +0100

    add sockets

--- server/server.js
+++ server/server.js
@@ -1,12 +1,28 @@
-const path = require('path'); //note this is a built in module so you don't need to download it
-const express = require("express");
-const publicPath = path.join(__dirname, "../public"); //this provide a path that is directly to the public folder
+//note in index.html we must add this     <script src = "/socket.io/socket.io.js"></script>
 
+const path = require('path'); //note this is a built in module so you don't need to download it
+const http = require('http'); //no need to install it, it is a node module
+const express = require("express");//this help us to create an http server
+const publicPath = path.join(__dirname, "../public"); //this provide a path that is directly to the public folder
+const socketIO = require("socket.io"); //this connect our server to our ui
 const port = process.env.PORT || 3000; //this make if we are using heroku it will check the port heroku provides
 const app = express(); //we are creating our app
+const server = http.createServer(app);
+const io = socketIO(server);//connect a server to the app
+
 
 app.use(express.static(publicPath)); //this is used to add the file in the public path into our app, this link dirctly to index.html
 
-app.listen(port, () => {
+io.on("connection", (socket) => {//this let u register an event listener
+console.log("NEw user connected");
+socket.on('disconnect', () => {//this runs when a socket is disconnected
+console.log("User was disconnected")
+})
+})
+
+
+
+
+server.listen(port, () => {
     console.log(`Server is up on ${port} 3000`)
 })
